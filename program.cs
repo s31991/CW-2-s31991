346af127c308f4b7aadc99c4f7c489d27b35867a
Luigi <Luigi@DESKTOP-5RHRIMC>
2025-03-25 20:59:38 +0100

    dodanie plikow projektu i przerobienie na aplikacje która działą a nie sam kod notatniku

--- program.cs
+++ program.cs
@@ -1,17 +1,12 @@
-class Program
-{
-    static void Main()
-    {
-        Ship ship = new Ship("Ocean Carrier", 50000, 100, 25);
-        var liquidContainer = new LiquidContainer(10000, true);
-        var gasContainer = new GasContainer(8000, 10);
-        var fridgeContainer = new RefrigeratedContainer(12000, "Bananas", 5);
+﻿// See https://aka.ms/new-console-template for more information
+Ship ship = new Ship("Ocean Carrier", 50000, 100, 25);
+var liquidContainer = new LiquidContainer(10000, true);
+var gasContainer = new GasContainer(8000, 10);
+var fridgeContainer = new RefrigeratedContainer(12000, "Bananas", 5);
 
-        ship.LoadContainer(liquidContainer);
-        ship.LoadContainer(gasContainer);
-        ship.LoadContainer(fridgeContainer);
+ship.LoadContainer(liquidContainer);
+ship.LoadContainer(gasContainer);
+ship.LoadContainer(fridgeContainer);
 
-        try { liquidContainer.Load(6000); } catch (Exception e) { Console.WriteLine(e.Message); }
-        ship.PrintShipInfo();
-    }
-}
+try { liquidContainer.Load(6000); } catch (Exception e) { Console.WriteLine(e.Message); }
+ship.PrintShipInfo();
